b82cb01e6aa7511f206d8a5c7f2e948444a3ddb7
Marylia Nieves <marylian@fullsail.edu>
2012-05-15 21:24:26 -0400

    Created an object and created 3 methods to that object.

--- Nieves_Marylia_assignment3.js
+++ Nieves_Marylia_assignment3.js
@@ -2,18 +2,57 @@
  * @author Mary
  */
 //alert("JavaScript works!");
+
 var procedureFunct={
-	procFunc: function(myBudget){
+    procFunc: function(myBudget){ //Procedure without any return.
+        if (myBudget<=25000){ 
+            console.log("This car is too expensive!! I cant afford it"); //Output if condition is met.
+        }
+        else {
+            console.log("This is within my budget but I am not sure about the color"); //Output if condition is not met.
+        }
+    },
+    boolFunc: function(vehicleMake, gasMileage){	
+		console.log("Which car is this and how much is the mileage?");
+	if (vehicleMake=="Hyundai" && gasMileage>=40){ 		//Comparision operation
+    	console.log("This is a "+vehicleMake+" and it gives "+gasMileage+" MPG"); //Output if condition is met.
+		return (booleanValue=true); //Boolean returned back.
+	}
+   	  else{
+         console.log("I would like to see a "+vehicleMake+" first.") //Output if condition is not met.
+         return (booleanValue=false); // Boolean returned back.
+		}
+	},
+	
+	numberFunc: function(carServiced){ //Number function
+        var mileLimit=20000;     //Local variable for the number function.
+        while (carServiced<mileLimit) { //while loop
+            carServiced+=5000; // This math will be executed for the number of times the condition is met.
+            console.log ("We recommend you service your car at "+carServiced+" miles"); //Output from the math.
+        }
+        mileService=20000;
+        return (mileService); //When the condition is not met, number is returned.
+}
 
 
 
 
+};
+
+
+procedureFunct.procFunc(25001);
 
 
 
+procedureFunct.boolFunc("Hyundai", 40);
 
 
- /*
+procedureFunct.numberFunc(0);
+
+
+
+/*
+
 var myBudget; //Declaration of numeric variable
 var procedureFunc;
 var vehicleMake; //Declaration of string variable
