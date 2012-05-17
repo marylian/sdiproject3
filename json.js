f0196a091723a41009f4de85e4b5f75c0c6adb89
Marylia Nieves <marylian@fullsail.edu>
2012-05-16 22:13:53 -0400

    Added json.js file with JSON data.

---
+++ json.js
@@ -0,0 +1,19 @@
+var carDealer={
+	"carMakes":[
+		{
+			"Manufacturer": "Audi",
+			"Model":"TT",
+			"Price": 45000
+		},
+		{
+			"Manufacturer": "Infinity",
+			"Model":"G37",
+			"Price": 47000
+		},
+		{
+			"Manufacturer": "Lexus",
+			"Model":"SC430",
+			"Price": 43000
+		}
+	]
+}
